b358687b0ca0d3e44b978b01a8fc336ecd377ef2
sucelius <sucelius@yandex.ru>
2023-03-14 15:16:40 +0300

    first iteration of typing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react';
 
+//import components
+import {Typing} from './components'
+
 function App() {
   return (
     <div className="App">
       <div>Hello</div>
+      <Typing></Typing>
     </div>
   );
 }
