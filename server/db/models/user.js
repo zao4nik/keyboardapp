d4f2aafc6a46d3bcdb7ddd28ea8abd5c235ca3e0
Илья Руденко <118446784+isii0@users.noreply.github.com>
2023-03-14 13:13:17 +0100

    database

---
+++ server/db/models/user.js
@@ -0,0 +1,26 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Game, UserText }) {
+      this.hasMany(Game, { foreignKey: 'userId' });
+      this.hasMany(UserText, { foreignKey: 'userId' });
+    }
+  }
+  User.init({
+    login: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
