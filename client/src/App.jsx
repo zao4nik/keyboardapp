5084c64b70b3feec7878691299e1196194488fb4
sucelius <sucelius@yandex.ru>
2023-03-14 10:35:04 +0300

    dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">Hello</div>
+    <div className="App">
+      <div>Hello</div>
+    </div>
   );
 }
 
