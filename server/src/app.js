47e4bbb838dc09713081a7f7c91fb9648beee996
Илья Руденко <118446784+isii0@users.noreply.github.com>
2023-03-14 08:28:05 +0100

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,11 @@
+require('dotenv').config(); //  импорт библиотеки dotenv
+
+const express = require('express'); // импорт библиотеки express
+
+const app = express(); // создаём экземпляр сервера
+
+const PORT = 3000; // задаем порт в переменную
+
+app.listen(PORT, () => { console.log(`server started on http://localhost:${PORT}`); }); // - проверяем работает ли сервер
+
+app.get('/', (req, res) => { res.send('Server works!!!'); }); // - проверяем эндпоинт на главной странице;
