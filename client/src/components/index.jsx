4bd6c381d578139d049cb5a52536f96ff7d16859
Илья Руденко <118446784+isii0@users.noreply.github.com>
2023-03-15 12:45:58 +0100

    clean up

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -2,7 +2,8 @@ import { Typing } from './Typing/Typing';
 import { Signin } from './Signin/Signin';
 import { Signup } from './Signup/Signup';
 import { Navbar } from './Navbar/Navbar';
+import Keyboard from './Keyboard/Keybord';
 
 export {
-  Typing, Signin, Signup, Navbar,
+  Typing, Signin, Signup, Navbar, Keyboard,
 };
