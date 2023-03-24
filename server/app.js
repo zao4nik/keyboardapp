371cc18076a742a76cbd0d10b21b227afa6a08bf
sucelius <sucelius@yandex.ru>
2023-03-24 11:20:48 +0300

    cors *

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const { Server } = require('socket.io');
 
 const io = new Server({
   cors: {
-    origin: '*',
+    origin: 'https://keyboardapp.netlify.app',
   },
 });
 
@@ -48,7 +48,7 @@ const sessionConfig = {
 };
 
 app.use(cors({
-  origin: '*',
+  origin: 'https://keyboardapp.netlify.app',
   credentials: true,
   // allowedHeaders: ['content-type'],
 }));
